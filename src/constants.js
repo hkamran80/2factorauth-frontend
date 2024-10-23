fe48b26c125b7f283b3ed53185cf453e2cb14318
H. Kamran <hkamran@hkamran.com>
2024-10-23 12:26:21 -0700

    Add environment variable support

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
-export const API_URL = "https://api.2fa.directory/frontend/v1";
-export const IMG_PATH = "https://api.2fa.directory/icons/"
+export const API_URL =
+  import.meta.env.VITE_API_URL || "https://api.2fa.directory/frontend/v1";
+export const IMG_PATH =
+  import.meta.env.VITE_IMG_URL || "https://api.2fa.directory/icons/";
